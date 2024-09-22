250026f810be6d7d1cea9971d3471acf7ac4a3d0
LuBeGar <164045264+LuBeGar@users.noreply.github.com>
2024-09-22 18:29:12 +0200

    Update saludo.php

--- ejercicio1/saludo.php
+++ ejercicio1/saludo.php
@@ -1,5 +1,7 @@
 <?php
+//Cogemos las variables del html
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
+//Mostramos el saludo con el nombre y el apellido
 echo "hola, $nombre $apellido";
 ?>
