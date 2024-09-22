844fb1d692f4bfd89d3d17959a650cfe8e67b502
LuBeGar <164045264+LuBeGar@users.noreply.github.com>
2024-09-22 17:19:51 +0200

    Add files via upload

---
+++ ejercicio4/dias.php
@@ -0,0 +1,26 @@
+<?php
+$numero = $_POST['numero'];
+switch($numero){
+    case 1: 
+        echo "Lunes";
+        break;
+    case 2:
+        echo "Martes";
+        break;
+    case 3:
+        echo "Miércoles";
+        break;
+    case 4:
+        echo "Jueves";
+        break;
+    case 5:
+        echo "Viernes";
+        break;
+    case 6:
+        echo "Sábado";
+        break;
+    case 7:
+        echo "Domingo";
+        break;
+}
+?>
