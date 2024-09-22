cfd6d4cd5bbbf54ffa5c95494e3c711b879c01ad
LuBeGar <164045264+LuBeGar@users.noreply.github.com>
2024-09-22 18:30:10 +0200

    Update viaje.php

--- ejercicio2/viaje.php
+++ ejercicio2/viaje.php
@@ -1,6 +1,8 @@
 <?php
 $distancia = $_POST['distancia'];
 $velocidad = $_POST['velocidad'];
+//Hacemos la operación
 $tiempo = $distancia / $velocidad;
+//Mostramos
 echo "El tiempo que tarda en recorrer la distancia de $distancia km a una velocidad de $velocidad km/h es de $tiempo h";
 ?>
