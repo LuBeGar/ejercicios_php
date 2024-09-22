62be85fe118d23e25a5b3caf6056d249d4448b03
LuBeGar <164045264+LuBeGar@users.noreply.github.com>
2024-09-22 18:32:45 +0200

    Update mayor.php

--- ejercicio3/mayor.php
+++ ejercicio3/mayor.php
@@ -2,12 +2,14 @@
 $numero1 = $_POST['numero1'];
 $numero2 = $_POST['numero2'];
 $numero3 = $_POST['numero3'];
+//Comprobamos cuál de los tres números es mayor
 if ($numero1 > $numero2 && $numero1 > $numero3){
     echo "El número mayor entre $numero1, $numero2 y $numero3 es $numero1";
 }else if ($numero1 <= $numero2 && $numero2 >= $numero3){
     echo "El número mayor entre $numero1, $numero2 y $numero3 es $numero2";
 }else if($numero1 <= $numero3 && $numero2 <= $numero3){
     echo "El número mayor entre $numero1, $numero2 y $numero3 es $numero3";
+//Por si se da la opción de que los tres números sean iguales
 }else if($numero1 == $numero2 && $numero1 == $numero3){
     echo "Los números son iguales";
 }
