804990dac54d4dd12012ae1d9315f26680aa76fa
LuBeGar <164045264+LuBeGar@users.noreply.github.com>
2024-09-25 17:39:31 +0200

    Update lista.php

--- ejercicio8/lista.php
+++ ejercicio8/lista.php
@@ -1,15 +1,17 @@
 <?php
-//Usamos explode para especificar por dónde queremos crear el array 
+//Usamos explode para especificar por dónde queremos separar los elementos insertados
 //(separador (en este caso usaremos los saltos de línea), String (lo que queremos que se añada))
-//trim para quitar los espacios y no se añadan productos vacíos
-$array = explode("\n", trim ($_POST['lista']));
+$array = explode("\r\n", ($_POST['lista']));
 
 //Creamos la lista
 echo "<ul>";
 //hacemos un bucle para recorrer el array hasta el número de elementos que haya (count)
 for ($i = 0; $i < count($array); $i++) {
     $linea = $array[$i];
+    //Para que no queden productos vacíos
+    if ($linea != null) {
     echo "<li>" . $linea . "</li>";
+    }
 }
 echo "</ul>";
 ?>
