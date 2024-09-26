c45a0a89278bf1b9b561a4846230627c6ff1e4fe
LuBeGar <164045264+LuBeGar@users.noreply.github.com>
2024-09-26 20:20:53 +0200

    Update tabla.php

--- ejercicio7/tabla.php
+++ ejercicio7/tabla.php
@@ -1,10 +1,12 @@
 <?php
 $numero = $_POST['numero'];
 $resultado = 0;
+
+echo "<table border = 1>";
 //Hacemos un bucle que vaya multiplicando el número desde 1 hasta 10
 for($i = 1; $i <= 10; $i++){
     $resultado = $numero*$i;
-    //Metemos el resultado en una tabla
-    echo "<table border = 1> <tr> <td> $numero x $i = $resultado </td> </tr> </table>"; 
+    echo " <tr> <td> $numero x $i = $resultado </td> </tr>"; 
 }
+echo "</table>";
 ?>
